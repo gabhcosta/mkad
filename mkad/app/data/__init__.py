d7fff4e4e324e17f9d71101340b7727c3183aeed
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-13 19:53:41 -0300

    add handling to csv data

---
+++ mkad/app/data/__init__.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import pandas as pd
+from mkad.app.helpers.data_manipulation import redistribute_vertices
+
+_mkad_coords_file_path= os.path.join(os.path.dirname(os.path.abspath(__file__)),'mkad_coordinates.csv')
+_mkad_coords_from_file= pd.read_csv(_mkad_coords_file_path, dtype='str').to_numpy(dtype=np.longdouble)
+
+
+MKAD_POLYGON_COORDS= redistribute_vertices(_mkad_coords_from_file, 200)
+
