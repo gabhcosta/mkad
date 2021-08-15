9f67b350868c23d02e0d5e97493cf328347ed231
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-15 14:54:58 -0300

    Add comments to resources.py

--- mkad/app/api/resources.py
+++ mkad/app/api/resources.py
@@ -7,7 +7,7 @@ from .response_handlers import handle_yandex_response
 from mkad.app.helpers.data_manipulation import build_address
 from mkad.app.helpers import input_checkers
 
-
+# Configuring Request Parser
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('country',  dest='country')
 post_parser.add_argument('provinces',  dest='provinces', action='append')
@@ -16,8 +16,15 @@ post_parser.add_argument('locality',  dest='locality')
 post_parser.add_argument('street',  dest='street')
 post_parser.add_argument('house',  dest='house')
 
+
 class DistanceFromMKAD(Resource):
+    """It interprets the payload arguments, makes a request to the Yandex API to get the latitude and longitude, and returns the distance from the point to MKAD. 
+    \n If the address passed is within MKAD. The distance is given as 0.
+    \n Only the http post method is allowed.
+    """
+    
     def post(self):
+        # Builds the required data
         args= post_parser.parse_args()
         input_checkers.has2arguments(args)
         address= build_address(args)
@@ -29,25 +36,36 @@ class DistanceFromMKAD(Resource):
             "format": "json",
             "lang": "en_US" 
         }
+
+        # Makes a request to the Yandex api
         r = requests.get('https://geocode-maps.yandex.ru/1.x/', params=payload)
 
-    
+        #Checks the location of the point
         response = handle_yandex_response(r)
         logging.info(f"{'-' * 47} END OF REQUEST {'-' * 46}")
         return response
 
+
 class Root(Resource):
+    """
+    Default answers for http methods in / 
+    """
+
     def get(self):
         return {'Server Status': 'Working!!'}
 
+
     def post(self):
         return {'Server Status': 'Working!!'}
+
     
     def put(self):
         return {'Server Status': 'Working!!'}
 
+
     def patch(self):
         return {'Server Status': 'Working!!'}
+
 
     def delete(self):
         return {'Server Status': 'Working!!'}
