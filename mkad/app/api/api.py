5b086e4a95b2ed9aa9e9b55436b0efa5f5f20116
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-15 14:54:42 -0300

    Add comments to api.py

--- mkad/app/api/api.py
+++ mkad/app/api/api.py
@@ -3,11 +3,13 @@ from flask import Flask, Blueprint
 from flask_restful import Api
 from .resources import DistanceFromMKAD, Root
 
-AppSettings.setup()
+AppSettings.setup() # Configuring global variables
+
+# API Configs
 app = Flask(__name__)
 api_bp = Blueprint('api', __name__)
 mkad_api = Api(api_bp)
-mkad_api.add_resource(Root, '/')
+mkad_api.add_resource(Root, '/') # Default answers for http methods in / 
 mkad_api.add_resource(DistanceFromMKAD, '/api')
 app.register_blueprint(api_bp)
 
