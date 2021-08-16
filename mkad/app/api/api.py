fc8284b370a0c84b52af60f9344facfc74afdcb5
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-16 20:58:32 -0300

    Modify urls

--- mkad/app/api/api.py
+++ mkad/app/api/api.py
@@ -1,7 +1,7 @@
 from mkad.settings import AppSettings
 from flask import Flask, Blueprint
 from flask_restful import Api
-from .resources import DistanceFromMKAD, Root
+from .resources import DistanceFromMKAD, DistanceFromGeneric, Root
 
 AppSettings.setup() # Configuring global variables
 
@@ -9,7 +9,8 @@ AppSettings.setup() # Configuring global variables
 app = Flask(__name__)
 api_bp = Blueprint('api', __name__)
 mkad_api = Api(api_bp)
-mkad_api.add_resource(Root, '/') # Default answers for http methods in / 
-mkad_api.add_resource(DistanceFromMKAD, '/api')
+mkad_api.add_resource(Root, '/', '/api') # Default answers for http methods in / 
+mkad_api.add_resource(DistanceFromMKAD, '/api/mkad')
+mkad_api.add_resource(DistanceFromGeneric, '/api/generic')
 app.register_blueprint(api_bp)
 
