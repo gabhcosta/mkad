042b6262e4606fb491844c7fe28b9f1ed75a125b
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-15 21:16:34 -0300

    Docstring + input validators

--- mkad/app/helpers/data_manipulation.py
+++ mkad/app/helpers/data_manipulation.py
@@ -5,21 +5,43 @@ def generate_shapely_point(point:str= None) -> Point:
     """This function is designed to work directly with the type of geo-coordinate response that the Yandex api gives. 
     \nIt takes a string "Lon Lat" and transforms it into a Point(lat, lon).
     """
+    
+    # Input Validators
+    if not isinstance(point, str):
+        raise ValueError(f'Expected type str. Got {type(point)}')
+
     lon= float(point.split()[0])
     lat= float(point.split()[1])
     return Point(lat, lon)
 
 
-def get_equidistant_points(p1, p2, parts):
+def get_equidistant_points(p1:np.ndarray= None, p2:np.ndarray= None, parts:int= None):
     """This function is responsible for distributing equidistant points along a line.
     """
+
+    # Input Validators
+    if not isinstance(p1, np.ndarray):
+        raise ValueError(f'Expected type np.ndarray. Got {type(p1)}')
+    if not isinstance(p2, np.ndarray):
+        raise ValueError(f'Expected type np.ndarray. Got {type(p2)}')
+    if not isinstance(parts, int):
+        raise ValueError(f'Expected type int. Got {type(parts)}')
+
     return zip(np.linspace(p1[0], p2[0], parts+1),
                np.linspace(p1[1], p2[1], parts+1))
 
 
-def redistribute_vertices(vertices, parts):
-    redistributed_vertices_array= np.empty((0,2), float)
+def redistribute_vertices(vertices:np.ndarray= None, parts:int= None) -> np.ndarray:
+    """This function is responsible for the distribution of equidistant points along the lines that compose the polygon boundary.
+    """
 
+    # Input Validators
+    if not isinstance(vertices, np.ndarray):
+        raise ValueError(f'Expected type np.ndarray. Got {type(vertices)}')
+    if not isinstance(parts, int):
+        raise ValueError(f'Expected type int. Got {type(parts)}')
+
+    redistributed_vertices_array= np.empty((0,2), float)
     for idx in range(len(vertices)-1):
         equidistant_points= np.array(
                                 list(get_equidistant_points(
@@ -35,7 +57,13 @@ def redistribute_vertices(vertices, parts):
 
     return np.unique(redistributed_vertices_array, axis=0)
 
-def build_address(args):
+def build_address(args:dict= None) -> str:
+    """This function transforms a dictionary with the address into a string, which represents the same information.
+    """
+    # Input Validators
+    if not isinstance(args, dict):
+        raise ValueError(f'Expected type dict. Got {type(args)}')
+
     _parts= [
         args.country, 
         ", ".join(args.provinces) if args.provinces else "",
@@ -49,6 +77,5 @@ def build_address(args):
     for p in _parts:
         if p:
             parts.append(p)
-
 
     return  ", ".join(parts)
