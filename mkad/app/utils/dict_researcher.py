7fbd03ff48d580ab1cd5de802186113a6854057f
Gabriel Costa <gabrielsantos.ghsc@gmail.com>
2021-08-15 21:02:51 -0300

    convert recursive_search to private

--- mkad/app/utils/dict_researcher.py
+++ mkad/app/utils/dict_researcher.py
@@ -10,22 +10,22 @@ class KeyResearcher():
       raise ValueError(f'Expected type str. Got {type(key)}')
 
     self.occurrences= list()
-    self.recursive_search(data, key)
+    self.__recursive_search(data, key)
     result= self.occurrences.copy()
     self.occurrences= list()
     return result
     
-  def recursive_search(self, data:dict=None, key:str=None) -> None:
+  def __recursive_search(self, data:dict=None, key:str=None) -> None:
     for k,v in data.items():
       if isinstance(v, list):
         for i in range(len(v)):
           if k == key:
             self.occurrences.append(v)
-          self.recursive_search(v[i], key)      
+          self.__recursive_search(v[i], key)      
       elif isinstance(v, dict):
         if k == key:
           self.occurrences.append(v)
-        self.recursive_search(v,key)
+        self.__recursive_search(v,key)
       else:
         if k == key:
           self.occurrences.append(v)
